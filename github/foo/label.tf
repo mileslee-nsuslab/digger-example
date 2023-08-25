f098079aa91fe1287e1f820dd9a00ed0835e72f0
mileslee-nsuslab <140376475+mileslee-nsuslab@users.noreply.github.com>
2023-08-25 16:38:46 +0900

    style: terraform fmt -recursive

--- github/foo/label.tf
+++ github/foo/label.tf
@@ -1,5 +1,5 @@
 resource "github_issue_label" "bar" {
-  repository  = "tfaction-example" 
+  repository  = "tfaction-example"
   name        = "bar-miles"
   color       = "FF0000"
   description = "bar"
