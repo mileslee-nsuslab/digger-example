5f69d27203badc1e556df4cdbe67675f4c1757bb
miles.lee <miles.lee@nsuslab.com>
2023-10-07 02:00:33 +0900

    chore: edit backend bucket name

--- terraform/agent/dev/global/client-user-verification/s3/main.tf
+++ terraform/agent/dev/global/client-user-verification/s3/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_version = ">= 1.0"
   backend "s3" {
     region = "us-east-1"        
-    bucket = "miles-tfaction-backend" 
+    bucket = "miles-test-bucket-blue" 
     key    = "terraform/agent/dev/global/client-user-verification/s3/v1/terraform.tfstate"
   }
 }
